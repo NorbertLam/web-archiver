c3386d387efb76536d4c1edde3cc4afff65c6514
Norbertlm <noobertlm@gmail.com>
2018-01-18 23:13:25 -0500

    Added documentation

--- server.go
+++ server.go
@@ -12,11 +12,13 @@ import (
 	"github.com/kelseyhightower/envconfig"
 )
 
+// A Page represents a webpage holding its url and its html layout.
 type Page struct {
 	Url  string `json:"url,omitempty"`
 	Html string `json:"html,omitempty"`
 }
 
+// A Key represents private information to be accessed.
 type Key struct {
 	AccountName   string
 	AccountKey    string
@@ -24,6 +26,7 @@ type Key struct {
 	ContainerName string
 }
 
+// Env holds a Key accessed from environment variables.
 type Env struct {
 	eKey Key
 }
@@ -32,12 +35,16 @@ var (
 	blobCli storage.BlobStorageClient
 )
 
+// Create page is a POST request which passes an Env and takes in data.
+// Data is read and saved in a Page struct.
+// BlockBlob operations are conducted, creating an empty BlockBlob.
+// Page data is uploaded to BlockBlob using PutBlock.
+// PutBlockList then commits the new BlockBlob to the database.
 func (env *Env) createPage(w http.ResponseWriter, req *http.Request) {
 	var p Page
 	decoder := json.NewDecoder(req.Body)
-	err := decoder.Decode(&p)
-	if err != nil {
-		panic(err)
+	if err := decoder.Decode(&p); err != nil {
+		log.Fatal(err)
 	}
 
 	pageTitle := p.Url + ".html"
@@ -54,14 +61,13 @@ func (env *Env) createPage(w http.ResponseWriter, req *http.Request) {
 	b.CreateBlockBlob(nil)
 
 	blockID := base64.StdEncoding.EncodeToString([]byte(pageTitle))
-	err = b.PutBlock(blockID, []byte(s), nil)
-	if err != nil {
-		fmt.Println("put block failed: %v", err)
+	if err := b.PutBlock(blockID, []byte(s), nil); err != nil {
+		fmt.Printf("put block failed: %v", err)
 	}
 
 	list, err := b.GetBlockList(storage.BlockListTypeUncommitted, nil)
 	if err != nil {
-		fmt.Println("get block list failed: %v", err)
+		fmt.Printf("get block list failed: %v", err)
 	}
 
 	uncommittedBlocksList := make([]storage.Block, len(list.UncommittedBlocks))
@@ -70,17 +76,17 @@ func (env *Env) createPage(w http.ResponseWriter, req *http.Request) {
 		uncommittedBlocksList[i].Status = storage.BlockStatusUncommitted
 	}
 
-	err = b.PutBlockList(uncommittedBlocksList, nil)
-	if err != nil {
-		fmt.Println("put block list failed: %v", err)
+	if err = b.PutBlockList(uncommittedBlocksList, nil); err != nil {
+		fmt.Printf("put block list failed: %v", err)
 	}
 
 }
 
+// Sets up env variables and opens connection to Azure blob storage client.
+// Initializes router with a handler for creating and uploading a html file to the Azure database.
 func main() {
 	var key Key
-	err := envconfig.Process("myserver", &key)
-	if err != nil {
+	if err := envconfig.Process("myserver", &key); err != nil {
 		fmt.Println(err)
 	}
 	env := &Env{eKey: key}
